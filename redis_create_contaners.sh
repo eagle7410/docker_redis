d4e9284db988f424c60db87eea6f749aa7346f01
Igor <verycooleagle@gmail.com>
2018-04-07 21:35:29 +0300

    Add srcipts for get image and create contaner.

---
+++ redis_create_contaners.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+ docker run --name redis -p 6379:6379 -d redis
+ docker ps
---
+++ unstall-local-redis.sh
@@ -0,0 +1,7 @@
+systemctl stop redis
+systemctl disable redis
+cd redis-stable
+sudo make uninstall
+rm /etc/redis/redis.conf
+rm -rf /var/lib/redis
+deluser redis
